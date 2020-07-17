0c8adfeef851db40b34f129fe66a202cef723a7e
Karl Koch <hi@kejk.co>
2020-07-17 15:25:24 +0100

    feat: updates to remove preset values and use placeholders instead, feat: updating to decimal pad

--- Px to Em/ContentView.swift
+++ Px to Em/ContentView.swift
@@ -11,6 +11,14 @@ import SwiftUI
 import Combine
 import UIKit
 
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
 struct AdaptsToSoftwareKeyboard: ViewModifier {
     
     @State var currentHeight: CGFloat = 0
@@ -23,7 +31,6 @@ struct AdaptsToSoftwareKeyboard: ViewModifier {
             .onAppear(perform: subscribeToKeyboardChanges)
     }
 
-    //MARK: - Keyboard Height
     private let keyboardHeightOnOpening = NotificationCenter.default
         .publisher(for: UIResponder.keyboardWillShowNotification)
         .map { $0.userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! CGRect }
@@ -33,8 +40,6 @@ struct AdaptsToSoftwareKeyboard: ViewModifier {
     private let keyboardHeightOnHiding = NotificationCenter.default
         .publisher(for: UIResponder.keyboardWillHideNotification)
         .map {_ in return CGFloat(0) }
-    
-    //MARK: - Subscriber to Keyboard's changes
     
     private func subscribeToKeyboardChanges() {
         
@@ -64,10 +69,14 @@ struct ContentView: View {
     @State private var baseText = "16"
     @State private var pixelText = "16"
     @State private var scaleText = "1.000"
+    @State private var baseTextEmpty = ""
+    @State private var pixelTextEmpty = ""
+    @State private var scaleTextEmpty = ""
     
     lazy var pixelInt = Double(pixelText) ?? 0
     lazy var baseInt = Double(baseText) ?? 16
     lazy var scaleInt = Double(scaleText) ?? 0
+    
     func pxToEms(baseInt: Double, pixelInt: Double, scaleInt: Double) -> Double {
         let emValue = (pixelInt / baseInt) * scaleInt
         return emValue
@@ -75,8 +84,6 @@ struct ContentView: View {
     
     let pxOrange = Color(red: 1.00, green: 0.60, blue: 0.00, opacity: 1.0)
     let pxTeal = Color(red: 0.00, green: 0.60, blue: 0.53, opacity: 1.0)
-   
-    // @State private var scaleSelector = 0
     
     var body: some View {
             VStack {
@@ -86,38 +93,47 @@ struct ContentView: View {
                         .font(.system(.largeTitle, design: .rounded))
                     .padding()
                     Text("Baseline pixel value").padding(.leading, 20.0).padding(.bottom, -16.0).padding(.top, 20.0).font(.headline)
-                    TextField(baseText, text: $baseText)
+                    TextField("16", text: $baseTextEmpty)
                     .font(.system(.title, design: .monospaced))
                     .padding(20)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color(red: 1.00, green: 0.60, blue: 0.00, opacity: 1.0), lineWidth: 3).padding(20))
+                    .keyboardType(.decimalPad)
                 VStack (alignment: .leading) {
                     HStack {
                         VStack (alignment: .leading) {
                         Text("Pixel value to convert").padding(.leading, 20.0).padding(.bottom, -16.0).padding(.top, 20.0).font(.headline)
-                        TextField(pixelText, text: $pixelText)
+                        TextField("16", text: $pixelTextEmpty)
                         .font(.system(.title, design: .monospaced))
                         .padding(20)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color(red: 0.00, green: 0.60, blue: 0.53, opacity: 1.0), lineWidth: 3).padding(20))
+                        .keyboardType(.decimalPad)
                         }
                         VStack (alignment: .leading) {
                         Text("Conversion scale").padding(.leading, 20.0).padding(.bottom, -16.0).padding(.top, 20.0).font(.headline)
-                        TextField(scaleText, text: $scaleText)
+                        TextField("1.000", text: $scaleTextEmpty)
                         .font(.system(.title, design: .monospaced))
                         .padding(20)
                         .textFieldStyle(RoundedBorderTextFieldStyle())
                         .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color(red: 0.00, green: 0.60, blue: 0.53, opacity: 1.0), lineWidth: 3).padding(20))
+                        .keyboardType(.decimalPad)
                         }
                     }
                 }
             }
             VStack (alignment: .center) {
                 Spacer()
-                Text("\(pixelText)px is \(String(format: "%.3f", pxToEms(baseInt: Double(baseText) ?? 16, pixelInt: Double(pixelText) ?? 0, scaleInt: Double(scaleText) ?? 0)))em")
+                Text("\(pixelTextEmpty)px is \(String(format: "%.3f", pxToEms(baseInt: Double(baseTextEmpty) ?? 16, pixelInt: Double(pixelTextEmpty) ?? 0, scaleInt: Double(scaleTextEmpty) ?? 0)))em")
                 .font(.system(.title, design: .monospaced)).bold()
                 Spacer()
                }.padding()
+                
+            VStack {
+                Button("Dismiss") {
+                    self.hideKeyboard()
+                }.padding()
+            }
         }.modifier(AdaptsToSoftwareKeyboard())
     }
 }
@@ -127,3 +143,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
