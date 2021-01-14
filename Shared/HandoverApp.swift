5d9bf17b923b01d048ccd2c00beb7fa48aadbe4f
Karl Koch <hi@kejk.co>
2021-01-14 20:07:41 +0000

    Initial Commit

---
+++ Shared/HandoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandoverApp.swift
+//  Shared
+//
+//  Created by Karl Koch on 14/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct HandoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
